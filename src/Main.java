09deb199516632c280a2700893a0ca2f0fc8d422
kuttubek <zhalalov98@internet.ru>
2022-08-15 16:22:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        set2.add(0);
+        set2.add(1);
+        set2.add(2);
+
+
+        symmetricSetDifference(set1, set2);
+    }
+
+    public static Set<Integer> symmetricSetDifference(Set<Integer> set1, Set<Integer> set2) {
+        //creating a new set
+        Set<Integer> newSet = new HashSet<Integer>(set1);
+        newSet.removeAll(set2);
+        set2.removeAll(set1);
+        newSet.addAll(set2);
+        System.out.println(newSet);
+        return newSet;
+    }
+}
+
+
+
+
